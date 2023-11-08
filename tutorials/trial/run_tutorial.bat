07b25c96549b93b52b51b05794200338444a1d67
Roberto Javier Escobar Mou <rescobar@ualberta.ca>
2023-11-07 23:31:29 -0700

    FilepathSetup

--- tutorials/trial/run_tutorial.bat
+++ tutorials/trial/run_tutorial.bat
@@ -2,17 +2,17 @@ ECHO off
 REM =============
 REM VARIABLES
 REM =============
-set PARAM_FP=%~dp0\parameters.ini
+set PARAM_FP=%~dp0\parameters_out11.ini
 
 REM default path for outputs
-set ROOT_DIR=C:\RICorDE-dev\outputs
+set ROOT_DIR=C:\Users\koysa\OneDrive\Documents\GitHub\RICorDE-hydro1\outputs
 
 REM =============
 REM SETUP
 REM =============
 REM activate pyqgis
-call "C:\Program Files\QGIS 3.22.8\ricorde\python-qgis-ltr_3228_activate.bat"
-
+#call "G:\My Drive\RESEARCH\RICorDE_evaluation_inputs_outputs_logs\python-qgis-ltr_3228_activation.bat"
+call "C:\Users\koysa\OneDrive\Documents\GitHub\RICorDE-hydro1\tutorials\trial\python-qgis-activate-20230112.bat"
  
 REM =============
 REM EXECUTE 
